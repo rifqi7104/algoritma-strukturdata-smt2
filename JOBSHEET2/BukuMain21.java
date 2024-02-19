5947aeaea0923460db0f123db2cf6b80c35bc3bf
rifqi7104 <rifqirizqullah7104@gmail.com>
2024-02-19 21:56:46 +0700

    JOBSHEET 2 DONE!

--- JOBSHEET2/BukuMain21.java
+++ JOBSHEET2/BukuMain21.java
@@ -17,5 +17,10 @@ public class BukuMain21 {
         Buku21 bk2 = new Buku21("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
+
+        Buku21 bukuRifqi = new Buku21("Sebuah Seni Untuk Bersikap Bodo Amat", "Mark Manson", 256, 20, 80000);
+        bukuRifqi.tampilInformasi();
+        bukuRifqi.gantiHarga(65000);
+        bukuRifqi.tampilInformasi();
     }
 }
