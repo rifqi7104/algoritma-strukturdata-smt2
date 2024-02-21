a3c38b6353f9973533263a1f3a06834e212da065
rifqi7104 <rifqirizqullah7104@gmail.com>
2024-02-21 20:24:59 +0700

    Proses program dragon

---
+++ JOBSHEET2/Dragon/Dragon21.java
@@ -0,0 +1,23 @@
+package JOBSHEET2.Dragon;
+
+public class Dragon21 {
+    int x,y,width,height;
+
+    void moveLeft(){
+        x -= 1;
+    }
+    void moveRight(){
+        x += 1;
+    }
+    void moveUp(){
+        y +=1;
+    }
+    void moveDown(){
+        y -=1;
+    }
+    void detectCollision(int x, int y){
+        if ((x < 0 || x >= width)||(y < 0 || y >= height)) {
+            System.out.println("GAME OVER!!");
+        }
+    }
+}
---
+++ JOBSHEET2/Dragon/DragonMain21.java
@@ -0,0 +1,27 @@
+package JOBSHEET2.Dragon;
+
+public class DragonMain21 {
+    public static void main(String[] args) {
+        Dragon21 dg1 = new Dragon21();
+        dg1.x = 3;
+        dg1.y = 5;
+        dg1.width = 10;
+        dg1.height = 10;
+    
+        dg1.moveRight();
+        dg1.detectCollision(dg1.x, dg1.y);    
+        dg1.moveRight();
+        dg1.detectCollision(dg1.x, dg1.y);    
+        dg1.moveRight();
+        dg1.detectCollision(dg1.x, dg1.y);    
+        dg1.moveLeft();
+        dg1.detectCollision(dg1.x, dg1.y);    
+        dg1.moveLeft();
+        dg1.detectCollision(dg1.x, dg1.y);    
+        for (int i = 1; i <= 10;) {
+            dg1.moveUp();
+            i++;   
+        }    
+        dg1.detectCollision(dg1.x, dg1.y);    
+    }
+}
